1312a3ba27e7fc7504b9b3e9f14040eff37609e5
Cainam <git@cainam.net>
2020-09-20 12:57:23 +0200

    Article time now prints correctly. Created two functions (one to store time, the other for basic layout). Fixed messy newline situation at end of last article.

--- NB_ThisJustIn.py
+++ NB_ThisJustIn.py
@@ -2,6 +2,25 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+def print_layout():
+    """Print out a basic layout at the top of the screen."""
+    # Basic layout for top of screen
+    print("--------------\n|Recent News:| \n--------------")
+    print("\n")
+
+
+def get_time_list(time_from_bs_obj):
+    """Takes all scraped time attributes and stores them in a list."""
+    # Iterate over 'time_from_bs_obj' and store every time in 'times_list'
+    # Also check the type is not None
+    times_list = []
+    for times in enumerate(time_from_bs_obj):
+        if times[1].get('data-number') is not None:
+            times_list.append(times[1].get('data-number'))
+    return times_list
+
+
 if __name__ == '__main__':
     # Prepare source for scraping
     source = requests.get('https://nieuwsblad.be').text
@@ -9,23 +28,20 @@ if __name__ == '__main__':
     # Initialize soup instance
     soup = BeautifulSoup(source, 'lxml')
 
-    # Basic layout for top of screen
-    print("|----------|\n|NET BINNEN|\n|----------|")
-    print("\n")
-
-    # Find all 7 "This just in" articles and put them in contents
+    # Find all 7 "This just in" articles and put them in 'contents'
     contents = soup.find_all('a', {'class': 'link-clean link-clean--delta'})
-    # Find the time the article went 'live'
+
+    # Find the time the article went 'live' and put it in 'time'
     time = soup.find_all('div', {'class': 'list-numbered__content'})
+
+    # Print basic layout
+    print_layout()
 
     # Iterate over all the articles and print out article, article number and link
     # Content[0] = index, Content[1] = article tags and attributes
     for content in enumerate(contents):
-        print("#" + str(content[0] + 1) + " " + content[1].get_text().strip() + ".")
+        print(get_time_list(time)[content[0]] + " #" + str(content[0] + 1) + " " + content[1].get_text().strip() + ".")
         print(content[1].get('href'))
-        print("\n")
-
-    # To Do: figure out how to correctly print out time article went 'live'
-    # for times in enumerate(time):
-    # if times[1] is not None:
-    # print(times[1].get('data-number'))
+        # Print a newline except if the last article has already been printed
+        if content[0] != 6:
+            print("\n")
